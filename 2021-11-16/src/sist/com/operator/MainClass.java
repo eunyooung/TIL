52ab15fe8c9fc1471626ab65cb6e6df3e1189fd8
Eunyoung Choi <choi327391@gmail.com>
2021-12-21 00:58:02 +0900

    feat: Record 2021-11-16 study

---
+++ 2021-11-16/src/sist/com/operator/MainClass.java
@@ -0,0 +1,41 @@
+package sist.com.operator;
+
+// 메모리(공간) 연산자(도구),제어자
+// 메소드(변수,연산자,제어문)
+// 최우선 ()우선순위변경, []배열선언 , .(주소링크)
+
+public class MainClass {
+    
+    public int firstOperator(int x,int y,int z) {      
+        //return x > y ? (x + y * z) : ((x + y) * z);
+        return x > y ? (x + y * z) : y < z ? ((x - y) * z):((x + y) * z);
+    }
+    
+    /*
+     * public boolean firstOperator(int x, int y, int z, int k) { 
+     *   return x > y ? x == z : y < k;
+     * }
+     */
+    
+    // 포도 딸기 바나나
+    public int firstOperator(int x,int y,int z,int k) {
+        //return x > y && z > k ? x + y * z : (x + y) * z;
+        return x > y || z > k ? x + y * z : (x + y) * z;
+    }
+    
+    public void firstOperator() {
+        //()우선순위 변경   []
+        //int []m; 배열선언시 사용 
+        //. 주소링크
+    }
+    
+    public static void main(String[] args) {
+        MainClass o = new MainClass();//HEAP
+        o.firstOperator();
+        //System.out.println(o.firstOperator(1, 2, 3, 4));//
+        /*
+         * int rs = o.firstOperator(5, 6, 7);//5,6,7 argumets
+         * System.out.printf("firstOperator Result=%d", rs);
+         */
+    }
+}
---
+++ 2021-11-16/src/sist/com/operator/Note.java
@@ -0,0 +1,20 @@
+package sist.com.operator;
+
+/*
+    Java
+    1. 메모리(Heap(new), Stack, Static(Class)), Variable, final(Constant)  
+        10,8,2,16진법  
+        PrimitiveType(기본형)  byte~ double char boolean, Cast 강제형,자동형  
+    
+    2. 연산자(Operator)  
+       계산을 목적으로 사용되는 기호 (우선순위)
+       최우선연산자,단항연산자 산술연산자 쉬프트연산자 (관계,비교)
+       논리연산자 삼항연산자 대입연산자 콤마연산자  
+    
+    3. 조건문,반복문  
+       if, if else, if elseif else, swtich,case default, for, while, do while, return, break, continue, label
+*/
+
+public class Note {
+
+}
