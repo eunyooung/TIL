bd128af676925ec6cf07edc4345ee00eeb11696d
eunyooung <choi327391@gmail.com>
2022-03-16 20:26:07 +0900

    fix: Fix email sending error

--- 2022-02-21(2)/src/main/java/sist/com/mail/MailSender.java
+++ 2022-02-21(2)/src/main/java/sist/com/mail/MailSender.java
@@ -10,7 +10,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import sist.com.data.input.ReserveVO;
-import sist.com.vo.*;
 
 public class MailSender {
     
@@ -20,9 +19,12 @@ public class MailSender {
 
     public static void naverMailSend(ReserveVO vo, String id) {
         
-        String host = "smtp.naver.com"; // 네이버일 경우 네이버 계정, gmail 경우 gmail 계정 
-        String user = ""; // 패스워드 
-        String password = ""; // SMTP 서버 정보를 설정한다. 
+        // 오류 참고!!!
+        // https://blog.naver.com/PostView.nhn?blogId=ambidext&logNo=222314812690
+        
+        String host = "smtp.naver.com"; // SMTP 서버 정보를 설정
+        String user = "sist_team1@naver.com"; // 네이버 계정
+        String password = ""; // 패스워드 
         Properties props = new Properties();
         props.put("mail.smtp.host", host);
         props.put("mail.smtp.port", 587);
@@ -35,27 +37,12 @@ public class MailSender {
         try {
             MimeMessage message = new MimeMessage(session);
             message.setFrom(new InternetAddress(user));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress("vcandjava@nate.com")); // 메일 제목 
-            message.setSubject(id + "님 예약 내역입니다!!"); // 메일 내용
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress("choi32739@naver.com")); // 메일 수신자
+            message.setSubject(id + "님 예약 내역입니다!!", "EUC-KR"); // 메일 제목
 
-            String html = "<html>\r\n" + "<head>\r\n" + "<meta charset=\"UTF-8\">\r\n"
-                    + "<title>Insert title here</title>\r\n"
-                    + "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css\">\r\n"
-                    + "<style type=\"text/css\">\r\n" + ".row{\r\n" + "  margin: 0px auto;\r\n" + "  width:350px;\r\n"
-                    + "}\r\n" + "</style>\r\n" + "</head>\r\n" + "<body>\r\n" + "  <div class=\"container\">\r\n"
-                    + "   <div class=\"row\">\r\n" + "    <table class=\"table\">\r\n" + "      <tr>\r\n"
-                    + "        <td class=\"text-center\" rowspan=\"5\">\r\n"
-                    + "         <img src=\"\" sytle=\"width:100%\">\r\n" + "        </td>\r\n" + "        <tr>\r\n"
-                    + "          <td colspan=\"2\"></td>\r\n" + "        </tr>\r\n" + "      </tr>\r\n"
-                    + "      <tr>\r\n" + "        <th width=25%>예약번호</th>\r\n" + "        <td width=75%>1</td>\r\n"
-                    + "      </tr>\r\n" + "      <tr>\r\n" + "        <th width=25%>예약일</th>\r\n"
-                    + "        <td width=75%>2022-03-10</td>\r\n" + "      </tr>\r\n" + "      <tr>\r\n"
-                    + "        <th width=25%>예약시간</th>\r\n" + "        <td width=75%>10:30</td>\r\n" + "      </tr>\r\n"
-                    + "      <tr>\r\n" + "        <th width=25%>예약인원</th>\r\n" + "        <td width=75%>5</td>\r\n"
-                    + "      </tr>\r\n" + "    </table>\r\n" + "   </div>\r\n" + "  </div>\r\n" + "</body>\r\n"
-                    + "</html>";
+            String html = "<div>예약완료</div>"; // 메일 내용
 
-            message.setContent(html, "text/html"); // send the message 
+            message.setContent(html, "text/html; charset=EUC-KR"); 
             Transport.send(message);
             System.out.println("Success Message Send");
         } catch (MessagingException e) {
